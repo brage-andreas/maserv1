33247d0dc82bfe07d0635a8660f4ba0dc6f8895f
hellobafu <brage.spammail@gmail.com>
2021-08-03 13:09:42 +0200

    .

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,13 +1,13 @@
 import { Interaction } from "discord.js";
 import { DaClient } from "../resources/definitions.js";
 
-export async function run(client: DaClient, itr: Interaction) {
-	if (itr.isCommand()) {
-		if (!itr.channel || itr.channel.type !== "GUILD_TEXT") {
-			itr.reply({ content: "Try this in a guild text channel!", ephemeral: true });
+export async function run(client: DaClient, interaction: Interaction) {
+	if (interaction.isCommand()) {
+		if (!interaction.channel || interaction.channel.type !== "GUILD_TEXT") {
+			interaction.reply({ content: "Try this in a guild text channel!", ephemeral: true });
 			return;
 		}
 
-		void client.commands.get(itr.commandName)?.run(client, itr);
+		void client.commands.get(interaction.commandName)?.run(client, interaction);
 	}
 }
